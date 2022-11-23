f8a2b2c195ada8472d57fb8064f8f856f02cf7fc
Javier David Nino Ruiz <103191427+JdavidN@users.noreply.github.com>
2022-11-22 23:26:48 -0500

    Add files via upload

---
+++ ejecutable/test.java
@@ -0,0 +1,17 @@
+package ejecutable;
+
+import controlador.Controlador;
+import modelo.Libro;
+import vista.VentanaPrincipal;
+
+public class test
+{
+    public static void main (String[] args)
+    {
+        VentanaPrincipal venPrin = new VentanaPrincipal();
+     
+        Libro model = new Libro("", "", "", 0, false);
+        Controlador miControlador = new Controlador(venPrin, model);
+        
+    }
+}
